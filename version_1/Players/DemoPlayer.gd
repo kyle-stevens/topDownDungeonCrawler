0e73a635dc6e147eac5e5496f0fe794f6e7f77a6
Kyle Stevens <43458322+kyle-stevens@users.noreply.github.com>
2022-04-17 13:33:33 -0700

    working on solid movement

--- version_1/Players/DemoPlayer.gd
+++ version_1/Players/DemoPlayer.gd
@@ -8,6 +8,7 @@ var vel : Vector3
 var move : Vector3
 var speed : int
 var target_location : Vector3
+var dir : Vector3
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	vel = Vector3()
@@ -28,24 +29,26 @@ func _process(delta):
 #	if Input.is_action_pressed("move_right"):
 #		move.z = 1
 #
-#	move.y = -1
-#	vel = move_and_slide(move * speed)
-	move.y = -1 #gravity
+	move.y = -1
 	vel = move_and_slide(move * speed)
 	
-	if (target_location - self.global_transform.origin) > Vector3(0.25,0,0.25):
-		vel = move_and_slide(target_location - self.global_transform.origin.normalized())
 	
+	#vel = move_and_slide(dir * speed)
+	self.global_transform.origin = self.global_transform.origin.move_toward(target_location, 0.5)
+	pass
+
+
+
+
+
+func _on_Area_input_event(camera, event, position, normal, shape_idx):
+	if event is InputEventMouseMotion:
+		return
 	
-func _input(event):
-	var ray_length = 100
-	if event is InputEventMouseButton and event.pressed and event.button_index == 1:
-		var camera = $Camera
-		var from = camera.project_ray_origin(event.position)
-		var to = from + camera.project_ray_normal(event.position) * ray_length
-		var cursorPos = Plane(Vector3.UP, 0).intersects_ray(from, to)
-		print(cursorPos)
-		
-		cursorPos = Vector3(cursorPos.x, 0, cursorPos.z)
-		target_location = cursorPos
-		#vel = move_and_slide(self.global_transform.origin + cursorPos)
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT and event.pressed:
+			pass
+			dir = position - self.global_transform.origin
+			target_location = position
+			dir = Vector3(dir.x, 0, dir.z)
+			print(dir)
